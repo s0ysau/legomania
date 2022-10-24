b68157080f97b91cd13124f3693f2aab0b8ac890
jerricklc@gmail.com <jerricklc@gmail.com>
2022-10-24 17:31:50 -0400

    Added controller .js files and connected to mongoDB

--- server.js
+++ server.js
@@ -4,13 +4,21 @@ const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
 const reviews = require('./models/reviews')
+const db = require('./models/db')
 
 // == Configure the app == //
-
+app.use((req, res, next) => {
+  res.locals.data = {}
+  next()
+})
+db.once('open', () => {
+  console.log('Connected to MongoDB')
+})
 
 
 // == Middleware == //
 app.use(methodOverride('_method'))
+app.use(express.static('public'))
 
 
 // == Middleware == //
